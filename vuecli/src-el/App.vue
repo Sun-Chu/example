b6dcc10e722b21c53917cc47921762c36c215a5a
zouh13 <1650139439@qq.com>
2019-11-07 11:22:29 +0800

    测试测试

---
+++ vuecli/src-el/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="app">
+  <Layout></Layout>
+</div>
+</template>
+
+<script>
+  import Layout from './layout';
+
+  export default {
+      data(){
+          return{}
+      }
+      ,components:{
+          Layout
+      }
+  }
+
+</script>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  html,body,#app,.el-container,.el-menu{
+    height: 100%;
+  }
+</style>
